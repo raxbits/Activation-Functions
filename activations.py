d46d86d64920cfdb42906f0b810ffe1160b98c20
Ramin Ranjbar <raxbits@gmail.com>
2017-08-06 17:52:56 -0400

    Add activation library file

---
+++ activations.py
@@ -0,0 +1,12 @@
+from math import e
+
+def sigmoid (logit):
+    return (1/(1+e**(logit*-1)))
+
+
+def relu(logit):
+    return(0 if (logit<0) else logit)
+
+def tanh (logit):
+    return ((e**logit - e**-logit)/(e**logit + e**-logit))
+
