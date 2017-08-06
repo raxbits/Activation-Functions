31e1b1537e656d268a6460713b14c3ffd41179de
Ramin Ranjbar <raxbits@users.noreply.github.com>
2017-08-06 17:55:31 -0400

    Update activations.py

--- activations.py
+++ activations.py
@@ -1,12 +1,12 @@
 from math import e
 
-def sigmoid (logit):
+def sigmoid(logit):
     return (1/(1+e**(logit*-1)))
 
 
 def relu(logit):
     return(0 if (logit<0) else logit)
 
-def tanh (logit):
+def tanh(logit):
     return ((e**logit - e**-logit)/(e**logit + e**-logit))
 
